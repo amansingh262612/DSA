3bcb8bd1d3c395c756bb20859e26e94295a095f0
Aman_Singh_auj <91659536+amansingh262612@users.noreply.github.com>
2024-03-23 22:40:27 +0530

    23.03.24

--- BASIC/Sorting.java
+++ BASIC/Sorting.java
@@ -16,8 +16,32 @@ public class Sorting {
         }
     }
 
+    public static void BubbleSortOptimal(int arr[]) {
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = 0; j <= n - 2 - i; j++) {
+                int swap = 0;
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swap = swap + 1;
+
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
     public static void main(String[] args) {
-        int arr[] = { 5, 4, 1, 3, 2 };
-        BubbleSort(arr);
+        // initial array
+        int arr[] = { 1, 2, 3, 5, 4 };
+        // BubbleSort(arr);
+        BubbleSortOptimal(arr);
+
     }
 }
